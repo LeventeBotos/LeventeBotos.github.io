bce5849553367e2a1db23247bec01de16e677680
Levente Botos <66298417+LeventeBotos@users.noreply.github.com>
2022-09-25 17:46:23 +0200

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-document.getElementById("RickRoll").style.display = "none";
-
-document.getElementById("Welcome").onclick = function() {
-  
-    document.getElementById("RickRoll").style.display = "block";
-    console.log("'Easter Egg'-et találtál.")
-
-}
