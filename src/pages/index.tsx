dcfd9bf3475dfedf4292ad73c8cddb75e5221a5a
Levente Botos <66298417+LeventeBotos@users.noreply.github.com>
2023-04-03 17:58:46 +0200

    index

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,12 @@
 export default function Home() {
   return (
-    <div className="bg-black">hi</div>
+    <div className="bg-black text-white text-xl lg:text-3xl  text-center">
+      <div className='h-screen p-10 font-bold flex-col items-center'>
+          <div className="self-center align-middle">
+            <div className="text-8xl md:text-9xl text-left md:text-center"><p>Hello,</p></div>
+            <div className="text-4xl lg:text-6xl text-left md:text-center"><p>I am Levente Botos</p></div>
+          </div>
+        </div>
+    </div>
   )
 }
