f31552525ab3aa9a82ba1580e0fddf91fa173f5b
Levente Botos <botos.levente2007@gmail.com>
2023-11-19 08:47:25 +0100

    rm

--- app/page.tsx
+++ app/page.tsx
@@ -9,13 +9,16 @@ const page = () => {
   return (
     <div className="  text-lg lg:text-xl text-center">
       <Hero />
-      <div className="p-5 flex flex-col gap-10 justify-evenly items-center">
+      <div className="flex w-full justify-evenly items-center">
+        <p>There'll be more.</p>
+      </div>
+      {/* <div className="p-5 flex flex-col gap-10 justify-evenly items-center">
         <p className="text-3xl font-bold gradient">Some of my projects</p>
         <RollingRoyals />
         <Volvid />
       </div>
       <Workflow />
-      <Stack />
+      <Stack /> */}
     </div>
   );
 };
